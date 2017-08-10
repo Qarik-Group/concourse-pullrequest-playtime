e62bd43ef9c12b4defa2b9730cffeaf544f92c4e
Dr Nic Williams <drnicwilliams@gmail.com>
2017-08-10 10:30:01 +1000

    first failed change

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo "Hello world. Submit a PR to change this missing."
-
-echo "Or submit a PR to change the exit to make this test pass or fail."
-exit 0
+echo "This is the first failed change"
+exit 1
